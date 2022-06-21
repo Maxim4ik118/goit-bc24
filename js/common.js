47f793ab4f46074f2ced4275dbf70dd1859453b6
Maxi <mmaximonchuk@gmail.com>
2022-06-21 16:59:37 +0300

    new: responsive sections

---
+++ js/common.js
@@ -0,0 +1,24 @@
+const openMenuBtnRef = document.querySelector('[data-open-menu-button]');
+const closeMenuBtnRef = document.querySelector('[data-close-menu-button]');
+
+const mobileMenuRef = document.querySelector('[data-menu]');
+
+openMenuBtnRef.addEventListener('click', () => {
+  const visible =
+    openMenuBtnRef.getAttribute('aria-visible') === 'true' || false;
+
+  openMenuBtnRef.setAttribute('aria-visible', visible);
+  document.body.style.overflow = 'hidden';
+
+  mobileMenuRef.classList.add('is-open');
+});
+
+closeMenuBtnRef.addEventListener('click', () => {
+  const visible =
+    closeMenuBtnRef.getAttribute('aria-visible') === 'true' || false;
+
+  closeMenuBtnRef.setAttribute('aria-visible', visible);
+  document.body.style.overflow = 'auto';
+
+  mobileMenuRef.classList.remove('is-open');
+});
