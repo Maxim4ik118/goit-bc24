e967295f681d408de23f0696c8876ec52417ffbc
Maxi <mmaximonchuk@gmail.com>
2022-06-21 22:09:51 +0300

    feat: responsive design

--- js/common.js
+++ js/common.js
@@ -1,24 +1,32 @@
 const openMenuBtnRef = document.querySelector('[data-open-menu-button]');
 const closeMenuBtnRef = document.querySelector('[data-close-menu-button]');
-
+const ctaHeroBtn = document.querySelector('.hero__btn');
 const mobileMenuRef = document.querySelector('[data-menu]');
 
-openMenuBtnRef.addEventListener('click', () => {
-  // const visible =
-  //   openMenuBtnRef.getAttribute('aria-visible') === 'true' || false;
+const TABLET_WIDTH = 767;
 
-  // openMenuBtnRef.setAttribute('aria-visible', visible);
+openMenuBtnRef.addEventListener('click', () => {
   document.body.style.overflow = 'hidden';
 
   mobileMenuRef.classList.add('is-open');
 });
 
 closeMenuBtnRef.addEventListener('click', () => {
-  // const visible =
-  //   closeMenuBtnRef.getAttribute('aria-visible') === 'true' || false;
-
-  // closeMenuBtnRef.setAttribute('aria-visible', visible);
   document.body.style.overflow = 'auto';
 
   mobileMenuRef.classList.remove('is-open');
 });
+
+window.addEventListener('resize', () => checkClientViewPort(this));
+
+checkClientViewPort(this);
+
+function checkClientViewPort(e) {
+  const userViewPortWidth = e.innerWidth;
+
+  if (userViewPortWidth > TABLET_WIDTH) {
+    ctaHeroBtn.classList.remove('c-btn--dark');
+  } else {
+    ctaHeroBtn.classList.add('c-btn--dark');
+  }
+}
